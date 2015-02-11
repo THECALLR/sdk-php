52c13932f663844f27384de2f94cb667c1a7697c
Florent CHAUVEAU <florent.chauveau+github@gmail.com>
2015-02-11 22:04:59 +0100

    formatting

--- examples/cli_create_realtime10.php
+++ examples/cli_create_realtime10.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Create a REALTIME10 Voice App
+ * Create a REALTIME10 Voice App (CLI)
  */
 
 require 'vendor/autoload.php';
@@ -9,9 +9,9 @@ if (count($argv) < 4) {
     die("{$argv[0]} api_login api_password realtime_url\n");
 }
 
-$login = $argv[1];
-$password = $argv[2];
-$URL = $argv[3];
+$login      = $argv[1];
+$password   = $argv[2];
+$URL        = $argv[3];
 
 $api = new \THECALLR\API\Client;
 $api->setAuthCredentials($login, $password);
